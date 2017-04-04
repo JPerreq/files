4a164d71c9f912af756e5f081cd0753f16a9c6a7
JPerreq <JPerreq@users.noreply.github.com>
2017-04-04 11:04:07 -0400

    Add header file

---
+++ writetext.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+   //create some data
+   short int a = -6730;
+   float b = 68.123; 
+   char c = 'J';
+ 
+   cout<<a<<" "<<b<<" "<<c;//display data to console (i.e. as formatted chars)
+
+   cout<<endl<<endl;
+ 
+   ofstream myfile{"abc.txt"};
+   
+   if (myfile)
+	{
+	myfile << a << " " << b << " " << c << endl;
+	}
+    else
+	{
+	cout << "ERROR-404. abc.txt will not open." << endl;
+	}
+
+myfile.close();
+
+   return 0;
+}
---
+++ writetext2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+   //create some data
+   short int a = -6730;
+   float b = 68.123; 
+   char c = 'J';
+ 
+   cout<<a<<" "<<b<<" "<<c;//display data to console (i.e. as formatted chars)
+
+   cout<<endl<<endl;
+ 
+   ofstream myfile{"abc.txt", ios::hex};
+   
+   if (myfile)
+	{
+	myfile.write(reinterpret_cast<char*>(&a), sizeof(a));
+	myfile.write(reinterpret_cast<char*>(&b), sizeof(b));
+	myfile.write(reinterpret_cast<char*>(&c), sizeof(c));
+	}
+    else
+	{
+	cout << "ERROR-404. abc.txt will not open." << endl;
+	}
+
+myfile.close();
+
+   return 0;
+}
